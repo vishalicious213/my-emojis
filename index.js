84b010ff355218ec7910850554adf555f74425e1
vishalicious213 <vishalicious213@gmail.com>
2023-01-11 18:16:36 -0800

    looping through myEmojis and rendering them in spans in #emoji-container

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 const myEmojis = ["👨‍💻", "⛷", "🍲"]
 const emojiContainer = document.getElementById("emojiContainer")
+
+for (let i = 0; i < myEmojis.length; i++) {
+    const emoji = document.createElement("span")
+    emoji.textContent = myEmojis[i]
+    emojiContainer.append(emoji)
+}
