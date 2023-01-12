a163f0b27d9c37dc951b701846167d2292104643
vishalicious213 <vishalicious213@gmail.com>
2023-01-11 21:17:13 -0800

    added eventListenet to push button that shows value of input field

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const myEmojis = ["👨‍💻", "⛷", "🍲"]
 const emojiContainer = document.getElementById("emoji-container")
+const emojiInput = document.getElementById("emoji-input")
+const pushBtn = document.getElementById("push-btn")
 
 for (let i = 0; i < myEmojis.length; i++) {
     const emoji = document.createElement("span")
     emoji.textContent = myEmojis[i]
     emojiContainer.append(emoji)
 }
+
+pushBtn.addEventListener("click", () => {
+    console.log(emojiInput.value)
+})
